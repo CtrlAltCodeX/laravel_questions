114ae3484b635cc887e68c9a8e2d1fd38fd88ff4
Kashish <kathuriak805@gmail.com>
2024-10-03 11:35:35 +0530

    image in question listing

--- resources/views/question-bank/index.blade.php
+++ resources/views/question-bank/index.blade.php
@@ -26,14 +26,13 @@
     }
 </style>
 @section('content')
-<div class="flex justify-between">
-    <h1 class="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white flex flex-col justify-between mb-0 items-center">Question
-        <button class="p-2 w-[40px] mr-5" id='delete-selected'>
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
-            </svg>
-        </button>
+<div class="flex flex-col">
+    <div class="flex flex-col ml-9">
+    <h1 class="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white flex flex-col justify-between ">Question
+
     </h1>
+</div>
+    <br/>
     <div class="flex flex-col items-self-end gap-2">
         <div class="flex justify-end items-center gap-2">
             <form action="{{ route('question.index') }}" method="GET" id='data' class="mb-0 flex gap-2">
@@ -87,6 +86,13 @@
                         <input type="hidden" name="sub_category_id" value="{{request()->sub_category_id}}" />
 
                         <div class="flex gap-2">
+                            <div class="justify-start mr-[400px]">
+                        <button class="p-2 w-[40px] " id='delete-selected'>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
+            </svg>
+        </button>
+</div>
                             <select class="block px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none cursor-pointer" name="per_page" id='per_page'>
                                 <option value="">Per Page</option>
                                 <option {{ request()->per_page == 50 ? 'selected' : '' }} value=50>50</option>
@@ -171,7 +177,7 @@
                 </th>
                 <th scope="col" class="p-2" data-column="id">ID</th>
                 <th scope="col" class="p-2" data-column="language">Language</th>
-                <th scope="col" class="p-2" data-column="image">Image</th>
+                <th scope="col" class="p-2" data-column="image">Images</th>
                 <th scope="col" class="p-2" data-column="question">Question</th>
                 <th scope="col" class="p-2" data-column="optionA">Option A</th>
                 <th scope="col" class="p-2" data-column="optionB">Option B</th>
@@ -181,7 +187,7 @@
                 <th scope="col" class="p-2" data-column="action">Action</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody> 
             {{-- @forelse($questions as $question)
             <tr>
                 <td class="p-2" data-column="id">
@@ -189,7 +195,8 @@
                 </td>
                 <td class="p-2" data-column="id">{{$question->id}}</td>
                 <td class="p-2" data-column="language">{{$question->language->name ?? $question->language->name}}</td>
-                <td class="p-2" data-column="image">{{$question->photo ?? ''}}</td>
+                <!-- <td class="p-2" data-column="image"><img src="{{$question->photo ?? ''}}"/></td> -->
+                <!-- <img src="{{$question->photo ?? ''}}"/> -->
                 <td class="p-2" data-column="question">{{$question->question ?? ''}}</td>
                 <td class="p-2" data-column="optionA">{{$question->option_a ?? ''}}</td>
                 <td class="p-2" data-column="optionB">{{$question->option_b ?? ''}}</td>
@@ -277,11 +284,8 @@
                     tbody.empty();
 
                     if (data.length > 0) {
-                        {{-- data.forEach(function(question) { --}}
                             var translated_questions = data;
-                            console.log(translated_questions);
                             if(translated_questions.length > 0){
-
                                 translated_questions.forEach(function(translated_question){
                                     var editUrl = `{{ route('question.edit', ':id') }}`.replace(':id', translated_question.question_id);
                                     var destroyUrl = `{{ route('question.destroy', ':id') }}`.replace(':id', translated_question.question_id);
@@ -292,7 +296,7 @@
                                             </td>
                                             <td class="p-2" data-column="id">${translated_question.question_id}</td>
                                             <td class="p-2" data-column="language">${translated_question.language.name}</td>
-                                            <td class="p-2" data-column="image">${translated_question.question.photo}</td>
+                                            <td class="p-2" data-column="image"><img src="${translated_question.question.photo}"  style='width: 50px; height: 50px; border-radius: 50%; object-fit: cover;'/></td>
                                             <td class="p-2" data-column="question">${translated_question.question_text}</td>
                                             <td class="p-2" data-column="optionA">${translated_question.option_a}</td>
                                             <td class="p-2" data-column="optionB">${translated_question.option_b}</td>
@@ -320,42 +324,6 @@
                                     tbody.append(newRow);
                                 });
                             }
-                            {{-- var editUrl = `{{ route('question.edit', ':id') }}`.replace(':id', question.id);
-                            var destroyUrl = `{{ route('question.destroy', ':id') }}`.replace(':id', question.id);
-                            var newRow = `
-                                <tr>
-                                    <td class="p-2" data-column="id">
-                                        <input type="checkbox" class="select-item" value="${question.id}" />
-                                    </td>
-                                    <td class="p-2" data-column="id">${question.id}</td>
-                                    <td class="p-2" data-column="language">${question.language.name}</td>
-                                    <td class="p-2" data-column="image">${question.photo}</td>
-                                    <td class="p-2" data-column="question">${question.question}</td>
-                                    <td class="p-2" data-column="optionA">${question.option_a}</td>
-                                    <td class="p-2" data-column="optionB">${question.option_b}</td>
-                                    <td class="p-2" data-column="optionC">${question.option_c}</td>
-                                    <td class="p-2" data-column="optionD">${question.option_d}</td>
-                                    <td class="p-2" data-column="level">${question.level}</td>
-                                    <td class="p-2 flex gap-2" data-column="action">
-                                        <a href="${editUrl}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
-                                                <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"></path>
-                                            </svg>
-                                        </a>
-                                        <form action="${destroyUrl}" method='POST'>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button href="#" class="font-medium text-danger dark:text-danger-500 hover:underline" onclick="return confirm('Are you sure?')">
-                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
-                                                    <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            `;
-                            tbody.append(newRow); --}}
-                        {{-- }); --}}
 
                         // Call toggleColumns to ensure the correct columns are shown/hidden
                         toggleColumns();
